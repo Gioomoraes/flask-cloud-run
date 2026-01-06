6180a63b0880f5d25b445af90de801a0542b4f6a
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2026-01-06 16:04:15 -0300

    Initial commit: Flask app deployed to Clou Run

---
+++ inventory.py
@@ -0,0 +1,15 @@
+inventory = [
+    {
+        "productid": "ABC123",
+        "onhandqty": "10"
+    },
+    {
+        "productid": "XYZ789",
+        "onhandqty": "20"
+    },
+    {
+        "productid": "LMN456",
+        "onhandqty": "30"
+    }
+
+]
